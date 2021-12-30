c766e021ddea41c802820b00a8cd3fbb6eb882e1
Alpha <massyalphaba@gmail.com>
2021-12-30 19:50:14 +0100

    ajout de code dans colonie.java

--- src/Plateau/Infrastructures/Colonie.java
+++ src/Plateau/Infrastructures/Colonie.java
@@ -1,37 +1,59 @@
 package Plateau.Infrastructures;
 import Joueur.Joueur;
 import Plateau.Composants.Case;
+import java.util.Scanner;
 
 public class Colonie {
 	private Joueur joueur;
-	private Case[] case_adjacente;
+	private Plateau plateau;
+	private int nbrRessource;
+	private LinkedList<Case> case_adja;
 	private boolean isVille;
 
-	public Colonie(Joueur j, boolean isVille){
+	public Colonie(Joueur j, boolean isVille, PLateau p){
 		this.joueur=j;
 		this.isVille=false;
-		this.case_adjacente=null;
-	}
-	
-	public void setCase(){
-	}
-	
-	public Joueur getJoueur(){
-		return this.joueur;
+		this.case_adja=null;
+		this.plateau =p;
+		this. nbrRessource=1;
 	}
 	
 	public void upgrade(){
 		this.isVille=true;
-		setCase();
+		this.nbrRessource=2;
 	}
 	
-	public boolean colonieNear(){ //renvoie vrai si une des routes d'une des case n'est pas nul est correspond à une route du joueur;
-		for(int i=0; i< case_adjacente.length; i++){
-			if((case_adjacente[i].getRoute("nord") !=null ||	case_adjacente[i].getRoute("sud") !=null || case_adjacente[i].getRoute("est") !=null || case_adjacente[i].getRoute("ouest") !=null) && (case_adjacente[i].getRoute("nord").getJoueur().getNom.equals(this.Joueur.getNom() || case_adjacente[i].getRoute("sud").getJoueur().getNom.equals(this.Joueur.getNom()|| case_adjacente[i].getRoute("est").getJoueur().getNom.equals(this.Joueur.getNom() ||case_adjacente[i].getRoute("ouest").getJoueur().getNom.equals(this.Joueur.getNom())){
-			return true;
+	public void decoupe(String str){
+		String [] tab= str.split(" ");
+		return str;
+	}
+	
+	
+	public void placer(){
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.pritnln("Où voulez-vous placer votre colonie");
+		String CaseLocation=sc.nextLine();
+		String []tab=decoupe(CaseLocation);
+		int i=Integer.parseInt(tab[0]);
+		int j=Integer.parseInt(tab[1]);
+		System.out.println("Dans quelle partie de la case voulez-vous placer votre colonie ?  haut gauche ?  bas gauche ? haut droit ? bas droit ?");
+		String location =sc.nextLine();
+		String [] decoupe=decoupe(location);
+		String locationHorizontale=decoupe[0];
+		String locationVerticale=decoupe[1];
+		LinkedList <Case> list=new LinkedList<>();
+		Case c= plateau[i][j];
+			for(var v : c.getMap().entrySet()){
+				if(v.getKey().equals(location) || v.getKey().equals(locationHorizontale) || v.getKey().equals(locationVerticale)){
+					list.add(v.getValue());
+				}
 			}
-		}
-		return false;
+		this.case_adja=list;
+	}
+	
+	public Joueur getJoueur(){
+		return this.joueur;
 	}
 	
 	
@@ -39,7 +61,8 @@ public class Colonie {
 		return this.isVille;
 	}
 	
-	public void placer(){
+	public LinkedList getCaseAdja(){
+		return this.case_adja;
 	}
 	
 	
